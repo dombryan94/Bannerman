b4b18fce83573e06c3820bac016027b94890eaaf
Dom Bryan <dombguitar@gmail.com>
2019-11-26 13:57:09 +0000

    Initial Commit

---
+++ Bannerman/Bannerman.h
@@ -0,0 +1,19 @@
+//
+//  Bannerman.h
+//  Bannerman
+//
+//  Created by Dom Bryan on 26/11/2019.
+//  Copyright © 2019 Dom Bryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bannerman.
+FOUNDATION_EXPORT double BannermanVersionNumber;
+
+//! Project version string for Bannerman.
+FOUNDATION_EXPORT const unsigned char BannermanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bannerman/PublicHeader.h>
+
+
